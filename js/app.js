dfa66edb7fb4ffd9dbcefecf879dbf16a366c7fb
Matheus Silva <theeus.souza@hotmail.com>
2016-09-28 12:27:55 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+
+$overlay.append($image);
+$overlay.append($caption);
+
+$("body").append($overlay);
+
+$("#gallery a").click(function(event){
+  event.preventDefault();
+var imageLocation = $(this).attr("href");
+
+$image.attr("src", imageLocation);
+$overlay.show();
+
+// var captionText = $(this).children("img").attr("alt");
+// $caption.text(captionText);
+});
+
+$overlay.click(function(){
+//  $(this).hide();
+  $overlay.hide();
+});
